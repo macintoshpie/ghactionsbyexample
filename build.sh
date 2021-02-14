b23d90f5f0058730b4591f4d1d9256fafa8a9e99
Ted Summer <ted.summer2@gmail.com>
2021-02-13 18:04:38 -0700

    feat: add build command for commiting public

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+set -e
+
 if [[ $1 == "watch" ]]; then
     ls ./examples/**/*.yml ./templates/*.tmpl ./public/site.css ./*.go | \
         entr docker run -it -v `pwd`:/go/src/app/ ghactionsbyexample
 else
     docker build . -t ghactionsbyexample && docker run -it -v `pwd`/public:/go/src/app/public/ ghactionsbyexample
+
+    if [[ $1 == "commit" ]]; then
+        echo "Commiting build..."
+        ref=$(git rev-parse --abbrev-ref HEAD)
+        if [[ $ref == "main" ]]; then
+            echo "Can't commit to main branch"
+            exit 1
+        fi
+
+        git add ./public
+        git commit -m "build: site built at $(date)"
+    fi
 fi
